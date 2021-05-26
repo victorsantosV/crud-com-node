62805967e804ce0cbec40c45ff5cdaca2be0240b
victorsantosV <37802657+victorsantosV@users.noreply.github.com>
2021-05-26 19:03:17 -0300

    Update FacebookController.js

--- src/controllers/FacebookController.js
+++ src/controllers/FacebookController.js
@@ -3,8 +3,8 @@ var Strategy = require('passport-facebook').Strategy;
 
 function facebook() {
     passport.use(new Strategy({
-            clientID: '206298000544809',
-            clientSecret: '658b9dd0f511fbf5a4d274eaf7b86675',
+            clientID: '',
+            clientSecret: '',
             callbackURL: 'http://localhost:3000/auth/facebook/callback'
         },
         function(accessToken, refreshToken, profile, cb) {
